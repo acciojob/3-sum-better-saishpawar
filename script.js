87a2fb779f710e4e25dcaeb12b5260ba97d2ca52
Saiisshh <118446344+saishpawar@users.noreply.github.com>
2023-03-22 20:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function threeSum(arr, target) {
 // write your code here
 
 	let n=arr.length;let Nearsum=1000;
-	for(let i=0;i<n;i++)
+	for(let i=0;i<n-2;i++)
 		{
-			for(let j=i+1;j<n;j++)
+			for(let j=i+1;j<n-1;j++)
 				{
 					for(let k=j+1;j<n;j++)
 						{
