2f034caca0951d346e53545fb3aab44cc7b99657
Saiisshh <118446344+saishpawar@users.noreply.github.com>
2023-03-22 19:52:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function threeSum(arr, target) {
 // write your code here
+
+	let n=arr.length;
+	for(let i=0;i<n;i++)
+		{
+			for(let j=i+1;j<n;j++)
+				{
+					for(let k=j+1;j<n;j++)
+						{
+							if(arr[i]+arr[j]+arr[k]===target)
+								return target;
+						}
+				}
+		}
   
 }
 
